a4308aa332c81906f4a0b2dbaa2cd470d4cea552
Wayne Luke <wayneluke@me.com>
2023-01-12 12:38:38 -0800

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,9 @@ $core='/volumes/secondary/sites/vbulletin/forum/core/';
 $log_errors=TRUE;
 
 if ($log_errors) {
-    unlink('./error_log.txt');
+    if (file_exists('./error_log.txt')) { 
+        unlink('./error_log.txt');
+    }
     error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
     ini_set('ignore_repeated_errors', TRUE); // always use TRUE
     ini_set('display_errors', TRUE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
